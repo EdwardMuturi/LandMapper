487826dfaa5d65896a522e191742700da84dc801
EdwardMuturi <kakaturi300@gmail.com>
2022-05-11 10:41:57 +0300

    Add initial files

---
+++ src/main/kotlin/com/bcf/landmapper/LandMapperApplication.kt
@@ -0,0 +1,11 @@
+package com.bcf.landmapper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LandMapperApplication
+
+fun main(args: Array<String>) {
+	runApplication<LandMapperApplication>(*args)
+}
---
+++ src/test/kotlin/com/bcf/landmapper/LandMapperApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bcf.landmapper
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LandMapperApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
